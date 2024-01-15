889621e2f0a30d4298ca1a190157a73f07d56f3c
sdfgx123 <sdfgx123@naver.com>
2024-01-15 15:31:02 +0900

    init.

--- ch6/6.1/learn-express/app.js
+++ ch6/6.1/learn-express/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+/**
+ * app.set() : 전역변수 같은 느낌으로 이해하면 된다
+ */
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
   // res.send('Hello, Express');
   res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.send('hello express');
 });
 
 app.listen(app.get('port'), () => {
